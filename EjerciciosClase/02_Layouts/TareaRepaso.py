9a4c19ee1858ce88a9badc366a2732d37a51b678
Cristian <cristian.perez.mail@gmail.com>
2023-11-29 14:02:04 +0100

    Ejercicio Repaso

---
+++ EjerciciosClase/02_Layouts/TareaRepaso.py
@@ -0,0 +1,72 @@
+﻿# Importamos las clases a utilizar del módulo QtWidgets del paquete PySide6
+from PySide6.QtWidgets import QApplication, QLabel, QWidget, QLineEdit, QPushButton, QFormLayout
+# Clases necesarias para los atajos de teclado
+from PySide6.QtGui import Qt, QKeySequence
+
+# Clase Ventana, hereda de QWidget, componente base
+class Ventana(QWidget):
+    # Constructor de la clase Ventana
+    def __init__(self):
+        # Llamada al constructor de la superclase
+        super().__init__()
+        
+        # Asignamos el título de la ventana
+        self.setWindowTitle("Login")
+        
+        # Modificamos el tamaño de la ventana
+        self.resize(300,150)
+        
+        # Creo un label para la salida
+        self.salida_label = QLabel(self)
+        
+        # Creo un QLineEdit para el usuario
+        self.usuario_input = QLineEdit()
+        self.usuario_input.setPlaceholderText("usuario")
+        # Creo un QLineEdit con el EchoMode en Password para la contra
+        self.contra_input = QLineEdit()
+        self.contra_input.setPlaceholderText("contraseña")
+        self.contra_input.setEchoMode(QLineEdit.EchoMode.Password)
+        # Creo un QPushButton para el login
+        login_button = QPushButton("Login", self)
+        
+        #Creamos el QFormLayout
+        form_layout = QFormLayout()
+        
+        form_layout.addRow('Usuario', self.usuario_input)
+        form_layout.addRow('Contraseña:', self.contra_input)
+        form_layout.addWidget(login_button)
+        form_layout.addWidget(self.salida_label)
+        
+        # Establecemos el layout principal para la ventana
+        self.setLayout(form_layout)
+        
+        # Al botón "login_button" le añado un atajo con la tecla "Enter". Al pulsar la tecla "Enter", el botón se activa.
+        login_button.setShortcut(QKeySequence(Qt.Key_Return))
+        
+        # Conectamos el evento 
+        login_button.clicked.connect(self.autentificar)
+    
+    # Creamos el método "modificar" que establece que texto de la "etiqueta" será el texto del item seleccionado del combo
+    def autentificar (self):
+        if (self.usuario_input.text() == "Admin" and self.contra_input.text() == "Admin"):
+            self.salida_label.setText("Usuario Correcto")
+            self.salida_label.setStyleSheet("""
+            color: "green";
+            """)
+        else:
+            self.salida_label.setText("Usuario Incorrecto")
+            self.salida_label.setStyleSheet("""
+            color: "red";
+            """)
+
+if __name__ == "__main__":
+    # Cada aplicación será una sola instancia de QApplication.
+    app = QApplication([])
+    # Creamos un objecto ventana.
+    ventana = Ventana()
+    # Mostramos la ventana, por defecto los componentes están ocultos.
+    ventana.show()
+    # Iniciamos el bucle de eventos.
+    app.exec()
+        
+        
---
+++ EjerciciosClase/05_QSS/Tarea09.py
@@ -0,0 +1,53 @@
+﻿#Modifica  los estilos de una casilla de verificación (QCheckBox) para que se muestre como un interruptor.
+#Los ficheros 1.png y 2.png serán las imágenes a modificar (adjuntadas como img en la tarea).
+
+import os
+from PySide6.QtWidgets import QApplication, QMainWindow, QCheckBox
+
+class Ventana(QMainWindow):
+    #Constructor de la clase Ventana
+    def __init__(self):
+        #Llamada al constructor de la superclase
+        super().__init__()
+        #Asignamos el título de la ventana
+        self.setWindowTitle("QSS CheckBox")
+
+        #Creamos el diccionario de imágenes
+        self.img_dict = {"checked":os.path.join(os.path.dirname(__file__),"1.png"), "unchecked":os.path.join(os.path.dirname(__file__),"2.png")}
+
+        #Creamos el elemento QCheckBox y le damos estilos a través de una función para hacerlo modular
+        self.interruptor = QCheckBox(self)
+        self.interruptor.setStyleSheet(self.estilo_interruptor())
+        
+        #Establecemos el elemento como Widget central
+        self.setCentralWidget(self.interruptor)
+        
+    #Creamos la función que devuelve los estilos (en el return) para el QCheckBox
+    def estilo_interruptor(self):
+        
+        return """ QCheckBox {
+            spacing: 5px;
+            }
+
+            QCheckBox::indicator {
+                width: 200px;
+                height: 250px;
+            }
+            QCheckBox::indicator:checked {
+                image: url(%s);
+            }
+            QCheckBox::indicator:unchecked {
+                image: url(%s);
+            } """ % (self.img_dict["checked"].replace("\\", "/"), self.img_dict["unchecked"].replace("\\", "/"))
+            #Las referencias al diccionario se le pasan por externa al comentario (%s) para que ejecuten su función
+
+if __name__ == "__main__":
+    #Cada aplicación será una sola instancia de QApplication.
+    app = QApplication([])
+    #Creamos un objecto ventana.
+    ventana = Ventana()
+    #Mostramos la ventana, por defecto los componentes están ocultos.
+    ventana.show()
+    #Iniciamos el bucle de eventos.
+    app.exec()
+        
