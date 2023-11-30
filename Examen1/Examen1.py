4881645dfe1e0d96c945c578379323cf266a16dc
Cristian <cristian.perez.mail@gmail.com>
2023-11-30 10:26:13 +0100

    Exámen 1 trimestre

---
+++ Examen1/Examen1.py
@@ -0,0 +1,78 @@
+﻿#Importamos las clases a utilizar
+import sys
+from PySide6.QtWidgets import QApplication , QMainWindow , QWidget , QFormLayout , QVBoxLayout , QLabel, QLineEdit, QPushButton
+# Clases necesarias para los atajos de teclado
+from PySide6.QtGui import Qt, QKeySequence
+
+#Clase Ventana, hereda de QWidget, componente base
+class Ventana(QWidget):
+    #Constructor de la clase Ventana
+    def __init__(self):
+        #Llamada al constructor de la superclase
+        super().__init__()
+        
+        #Asignamos el título de la ventana
+        self.setWindowTitle("Registro de Empleados")
+        
+        #Modificamos el tamaño de la ventana
+        self.resize(300,150)
+        
+        #Creamos los elementos que se usarán en la ventana
+        self.nombre_input = QLineEdit()
+        self.edad_input = QLineEdit()
+        self.direccion_input = QLineEdit()
+        self.telefono_input = QLineEdit()
+        self.cargo_input = QLineEdit()
+        self.salario_input = QLineEdit()
+        
+        registrar_button = QPushButton("Registrar", self)
+        
+        self.registro_label = QLabel("Empleados Registrados:", self)
+        
+        #Creamos los Layouts que se van a usar
+        princ_vlayout = QVBoxLayout()
+        form_layout = QFormLayout()
+        self.secun_vlayout = QVBoxLayout()
+
+        #Añadimos los elementos al form_layout
+        form_layout.addRow("Nombre: ", self.nombre_input)
+        form_layout.addRow("Edad: ", self.edad_input)
+        form_layout.addRow("Dirección: ", self.direccion_input)
+        form_layout.addRow("Teléfono: ", self.telefono_input)
+        form_layout.addRow("Cargo: ", self.cargo_input)
+        form_layout.addRow("Salario: ", self.salario_input)
+        
+        #Añadimos los elementos al princ_vlayout
+        princ_vlayout.addLayout(form_layout)
+        princ_vlayout.addWidget(registrar_button)
+        princ_vlayout.addWidget(self.registro_label)
+        princ_vlayout.addLayout(self.secun_vlayout)
+        
+        # Establecemos el layout principal para la ventana
+        self.setLayout(princ_vlayout)
+        
+        # Al botón "login_button" le añado un atajo con la tecla "Enter". Al pulsar la tecla "Enter", el botón se activa.
+        registrar_button.setShortcut(QKeySequence(Qt.Key_Return))
+        
+        # Conectamos el evento 
+        registrar_button.clicked.connect(self.registrarEmpleados)
+        
+    def registrarEmpleados(self):
+        
+        self.secun_vlayout.addWidget(QLabel(f"{self.nombre_input.text()} - {self.cargo_input.text()} - {self.salario_input.text()}"))
+        self.nombre_input.clear()
+        self.edad_input.clear()
+        self.direccion_input.clear()
+        self.telefono_input.clear()
+        self.cargo_input.clear()
+        self.salario_input.clear()
+
+if __name__ == "__main__":
+    # Cada aplicación será una sola instancia de QApplication.
+    app = QApplication([])
+    # Creamos un objecto ventana.
+    ventana = Ventana()
+    # Mostramos la ventana, por defecto los componentes están ocultos.
+    ventana.show()
+    # Iniciamos el bucle de eventos.
+    app.exec()
---
+++ Examen1/Examen2.py
@@ -0,0 +1,76 @@
+﻿#Importamos las clases a utilizar
+import os
+from PySide6.QtCore import Qt
+from PySide6.QtGui import QAction, QIcon, QKeySequence
+from PySide6.QtWidgets import (QApplication, QMainWindow, QToolBar, QWhatsThis, QDockWidget, QLabel, QTextEdit)
+
+#Clase Ventana, hereda de QWidget, componente base
+class Ventana(QMainWindow):
+    #Constructor de la clase Ventana
+    def __init__(self):
+        #Llamada al constructor de la superclase
+        super().__init__()
+        
+        #Asignamos el título de la ventana
+        self.setWindowTitle("¿Cual es la asignatura más importante?")
+        
+        #Modificamos el tamaño de la ventana
+        self.resize(500,500)
+        
+        #Creamos el diccionario de imágenes
+        self.img_dict = {"ayuda":os.path.join(os.path.dirname(__file__),"ayuda.png"), "libros":os.path.join(os.path.dirname(__file__),"libros.png")}
+        
+        #Creamos la accón Prioridad... le damos el atajo de teclado y hacemos la conexión
+        acc_prio = QAction(QIcon(self.img_dict["libros"]),"Prioridad de asignaturas\n 2ºDAM", self)
+        acc_prio.setShortcut(QKeySequence("Ctrl+p"))
+        acc_prio.setStatusTip("Prioridad de asignaturas 2ºDAM")
+        acc_prio.setWhatsThis("Nos indica cual es la asignatura más importante de 2ºDAM")
+        acc_prio.triggered.connect(self.prioridadAsignaturas)
+        
+        #Creamos la acción Ayuda, hacemos la conexión a la acción
+        acc_ayuda = QAction(QIcon(self.img_dict["ayuda"]),"Ayuda", self)
+        acc_ayuda.setShortcut(QKeySequence("Shift+F1"))
+        acc_ayuda.setStatusTip("Ayuda")
+        acc_ayuda.setWhatsThis("Activamos el modo ayuda")
+        acc_ayuda.triggered.connect(self.ayuda)
+        
+        #Creamos el objeto de Barra de Menús, le damos un atajo de teclado con "&" y le añadimos las acciones
+        barra_menus = self.menuBar()
+        menu = barra_menus.addMenu("&Asignaturas")
+        menu.addAction(acc_prio)
+        menu.addAction(acc_ayuda)
+        
+        #Creamos la barra de herramientas, le damos estilos a los botones y le añadimos las acciones
+        barra_herramientas = QToolBar("Barra de herramientas")
+        barra_herramientas.setToolButtonStyle(Qt.ToolButtonTextUnderIcon)
+        barra_herramientas.addAction(acc_prio)
+        barra_herramientas.addAction(acc_ayuda)
+        self.addToolBar(barra_herramientas)
+        
+        #Creamos el componente base del Dock
+        dock = QDockWidget()
+        dock.setWindowTitle("El subsconsciente del alumno")
+        self.texto=QTextEdit("")
+        dock.setWidget(self.texto)
+        dock.setMinimumWidth(100)
+        dock.setMinimumHeight(100)
+        self.addDockWidget(Qt.TopDockWidgetArea, dock)
+        
+    def prioridadAsignaturas(self):
+        self.texto.insertPlainText("La asignatura más importante de 2ºDAM es Desarrollo de interfaces.\n")
+        
+    def ayuda(self):
+        if QWhatsThis.inWhatsThisMode():
+            QWhatsThis.leaveWhatsThisMode()
+        else:
+            QWhatsThis.enterWhatsThisMode()
+
+if __name__ == "__main__":
+    #Cada aplicación será una sola instancia de QApplication.
+    app = QApplication([])
+    #Creamos un objecto ventana.
+    ventana = Ventana()
+    #Mostramos la ventana, por defecto los componentes están ocultos.
+    ventana.show()
+    #Iniciamos el bucle de eventos.
+    app.exec()
