da50b747add9cf75956547edf052f1988ac3d9a7
Cristian <125401680+Tropezodev@users.noreply.github.com>
2024-01-17 18:26:33 +0100

    edit

--- EjerciciosClase/06_Componentes/Componente3_Empresa/Empresa.py
+++ EjerciciosClase/06_Componentes/Componente3_Empresa/Empresa.py
@@ -13,21 +13,23 @@ from PySide6.QtCore import Qt, Signal, Property
 
 class QWidgetMod(QWidget):
     
-    #Creamos la señal
+    #Creamos la señal que se emitirá al hacer doble click
     double_click = Signal(str)
   
-    #Constructor de la clase Ventana
+    #Constructor de la clase QWidgetMod
     def __init__(self, empresa_dict:str, parent=None):
+        
         #Llamada al constructor de la superclase
         super().__init__()
         
         #Asignamos el título de la ventana
         self.setWindowTitle("Componente Empresa")
         
-        #Creamos los elementos del componente
+        #Creamos los elementos del componente en referencia al diccionario que se va a obtener por parámetro en el constructor
         img = QPixmap(empresa_dict["img"])
         img_lb=QLabel(self)
         img_lb.setPixmap(img)
+        
         name = QLabel(empresa_dict["name"])
         address = QLabel(empresa_dict["adress"])
         
@@ -48,15 +50,19 @@ class QWidgetMod(QWidget):
         #Juntamos los String de los elementos a uno solo
         self.data = f'-Rutal del icono: {empresa_dict["img"]}\n -Nombre de la empresa: {empresa_dict["name"]}\n -Dirección de la empresa: {empresa_dict["adress"]}'
         
+    #Redefinimos la función mouseDoubleClickEvent (override) para que emita la señal double_click
     def mouseDoubleClickEvent(self,e):
-        #Conectamos la señal
+        
+        #Conectamos la señal y emitimos con los datos del componente
         self.double_click.emit(self.data)
 
-# Para probarlo creamos una clase ventana que llame al componenete
-# Clase Ventana, hereda de QWidget, componente base
+# Para probar el componente creamos una clase ventana que llame al componenete
+# Clase Ventana, hereda de QMainWindow, componente base
 class Ventana(QMainWindow):
+    
     # Constructor de la clase Ventana
     def __init__(self):
+        
         # Llamada al constructor de la superclase
         super().__init__()
         
@@ -66,17 +72,23 @@ class Ventana(QMainWindow):
         #Creamos el diccionario que le vamos a pasar por parámetro
         empresa_dict= {"img":os.path.join(os.path.dirname(__file__),"Pepo.png"),"name":"PepoInc","adress":"Calle Falsa 123"}
         
+        #Instanciamos el componente QWidgetMod con el diccionario como parámetro
         empresa = QWidgetMod(empresa_dict)
         
+        #Establecemos el componente como widget central
         self.setCentralWidget(empresa)
 
-
+#Punto de entrada para la aplicación
 if __name__ == "__main__":
+    
     # Cada aplicación será una sola instancia de QApplication.
     app = QApplication([])
+    
     # Creamos un objecto ventana.
     ventana = Ventana()
+    
     # Mostramos la ventana, por defecto los componentes están ocultos.
     ventana.show()
+    
     # Iniciamos el bucle de eventos.
     app.exec()
