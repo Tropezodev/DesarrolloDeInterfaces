671f20aeb6106ee8d3d64af411fe0d9afab9e7e3
Cristian <125401680+Tropezodev@users.noreply.github.com>
2024-01-31 13:09:39 +0100

    Preparación Exámen

---
+++ Examen2/Components/Componente.py
@@ -0,0 +1,12 @@
+﻿from PySide6.QtCore import Signal
+from PySide6.QtWidgets import QLineEdit
+
+
+class QPassword(QLineEdit):
+    #Creamos la señal que se emitirá al hacer """"""
+    #"""""" = Signal(""""")
+  
+    def __init__(self):
+        super().__init__()
+        
+        
---
+++ Examen2/Examen.py
@@ -0,0 +1,30 @@
+﻿
+
+
+#Importamos el componente
+from PySide6.QtWidgets import QApplication, QMainWindow
+from Components.Componente import 
+
+class Ventana(QMainWindow):    
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("")
+        self.resize(300,150)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    ventana = Ventana()
+    ventana.show()
+    app.exec()
